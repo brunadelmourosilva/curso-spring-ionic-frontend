e5dac80cd0ccfb7b546ba51f54ed08d876a69f32
Bruna Delmouro <brunadelmouro@gmail.com>
2022-02-21 16:44:51 -0300

    Criando um interceptor para tratamento de erros

--- src/pages/categories/categories.ts
+++ src/pages/categories/categories.ts
@@ -35,9 +35,7 @@ export class CategoriesPage {
       response => {
           this.items = response;
       },
-      error => {
-        console.log(error);
-      });
+      error => {});
   }
 
 }
